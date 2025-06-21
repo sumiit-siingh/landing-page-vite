a57ecf96bdd8fd9f609bf6dff8309b3f87ac0c17
Sumit Singh <sumitsinghsakarari@gmail.com>
2025-06-21 22:24:33 +0530

    intial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+function App() {
+
+
+  return (
+    <div className="h-screen w-screen bg-red-400 relative">
+      <img src="https://res.cloudinary.com/dq829orud/image/upload/v1750523648/20250619_1044_Grain_Gradient_Sphere_remix_01jy39njjefmdr1fmd6py1dzyx_em2dqd.png" className="w-full h-full object-cover" />
+      <div className="absolute top-10 left-1/2 flex items-center justify-center text-white text-4xl">
+        hi
+      </div>
+    </div>
+  )
+}
+
+export default App
